ff191189511aa97e2146ebe00b077d9339fa5924
Alessandro Lucarini <dixiedream@hotmail.it>
2019-10-28 14:35:06 +0100

    Update APIError.js

--- src/shared/errors/APIError.js
+++ src/shared/errors/APIError.js
@@ -1,1 +1,11 @@
-console.log('Nothing');
+/**
+ * The base module for handling errors, every other error has to extends this
+ */
+module.exports = class APIError extends Error {
+  constructor(message) {
+    super(message);
+    this.message = this.message;
+    this.type = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+};
