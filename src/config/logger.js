2dd9e8dbbe8260d99b33e3d675872688e419ed5c
Alessandro Lucarini <alessandro.lucarini@smanapp.com>
2019-11-03 19:19:54 +0100

    Ok course buyed

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,7 +2,7 @@ const { createLogger, format, transports } = require("winston");
 require("winston-mongodb");
 require("express-async-errors");
 
-const { SERVICE_NAME, MONGO__LOG_CONNECTION, NODE_ENV } = process.env;
+const { SERVICE_NAME, MONGO__LOG_CONNECTION } = process.env;
 
 const logger = createLogger({
   level: "info",
@@ -15,44 +15,23 @@ const logger = createLogger({
   defaultMeta: { service: SERVICE_NAME || "Expressjs" },
   transports: [
     /**
-     * - Write to all logs with level `info` and below to `combined.log`.
-     * - Write all logs `error` (and below) to `error.log`.
+     * - Write to all logs with level `info` and below to the Console.
      * - Write all logs with level `info` and below to MongoDB
      */
-    new transports.File({
-      filename: "logs/error.log",
-      level: "error"
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple())
     }),
-    new transports.File({ filename: "logs/combined.log" }),
     new transports.MongoDB({
       db: MONGO__LOG_CONNECTION || "mongodb://db:27017/expressmongo_log",
       format: format.combine(format.json(), format.metadata())
     })
   ],
   exceptionHandlers: [
-    /**
-     * - Write occurred exceptions to `exceptions.log`
-     */
-    new transports.File({ filename: "logs/exceptions.log" })
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple())
+    })
   ]
 });
-
-/**
- * If we're not in production then **ALSO** log to the `console`
- * with the colorized simple format.
- */
-if (NODE_ENV !== "production") {
-  logger.add(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple())
-    })
-  );
-  logger.exceptions.handle(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple())
-    })
-  );
-}
 
 process.on("unhandledRejection", ex => {
   throw ex;
