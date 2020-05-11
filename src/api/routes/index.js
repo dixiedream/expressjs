1209af9633504de237caaeb307c6f006c9dd54da
Alessandro Lucarini <dixiedream@hotmail.it>
2020-05-11 21:42:43 +0200

    Removed unused require

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const router = require("express").Router();
 const error = require("../../middleware/error");
 const profiler = require("../../middleware/profiler");
 
