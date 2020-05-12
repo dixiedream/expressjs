44f353fddb0229ea8f99381f8b83e948de97406c
Alessandro Lucarini <dixiedream@hotmail.it>
2020-05-12 15:12:46 +0200

    Fixed helmet not updating headers

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const app = express();
-require("./src/api/routes/index")(app);
-require("./src/config/db")();
 
 if (process.env.NODE_ENV === "production") {
   // eslint-disable-next-line global-require
   require("./src/config/prod")(app);
 }
+
+require("./src/api/routes/index")(app);
+require("./src/config/db")();
 
 module.exports = app;
