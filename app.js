8e26f28da3f97cfcbc50123d3bc268abe74ba0ea
Alessandro <dixiedream88@gmail.com>
2022-05-16 19:25:45 +0200

    Madness start

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const profiler = require("./src/middleware/profiler");
 
 const app = express();
@@ -9,8 +10,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //  Middlewares
+app.use(cookieParser);
 app.use(profiler);
-
+app.use(express.json({ limit: "1mb" })); // Change limit body size
 /**
  * Healthcheck route
  */
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,38 +8,53 @@ const logger = require("../config/logger");
 
 const { JWT_PRIVATE_KEY, JWT_ISSUER } = process.env;
 
+function verifyJWT(token) {
+  return jwt.verify(token, JWT_PRIVATE_KEY, {
+    algorithms: ["HS256"],
+    issuer: [JWT_ISSUER],
+  });
+}
+
 const auth = async (req, res, next) => {
-  let token = req.header("Authorization");
+  const { accessToken, refreshToken } = req.cookies;
+
   try {
-    if (!token) {
+    if (!accessToken) {
       throw new MissingTokenError();
     }
 
-    token = token.replace("Bearer ", "");
-    const data = jwt.verify(token, JWT_PRIVATE_KEY, {
-      algotithms: ["HS256"],
-      issuer: [JWT_ISSUER],
-    });
-    const user = await User.findOne({ _id: data._id });
-    if (!user) {
-      throw new NotAuthorizedError();
+    try {
+      const data = verifyJWT(accessToken);
+
+      const user = await User.findOne({ _id: data._id });
+      if (!user) {
+        throw new NotAuthorizedError();
+      }
+
+      req.user = user;
+      logger.info("USER_AUTHORIZED", { email: user.email });
+      next();
+    } catch (error) {
+      // Access token is expired
+      if (!refreshToken) {
+        throw new MissingTokenError();
+      }
+
+      const data = verifyJWT(refreshToken);
     }
-    req.user = user;
-    req.token = token;
-    logger.info("USER_AUTHORIZED", { email: user.email });
-    next();
   } catch (error) {
+    const { name, message } = error;
     logger.info("AUTHORIZATION_FAILED", {
-      errorName: error.name,
-      errorMessage: error.message,
+      errorName: name,
+      errorMessage: message,
     });
     if (
       error instanceof NotAuthorizedError ||
       error instanceof MissingTokenError
     ) {
-      res.status(401).send({ type: error.type, message: error.message });
+      res.status(401).send({ type: error.type, message });
     } else if (error instanceof APIError) {
-      res.status(401).send({ type: error.type, message: error.message });
+      res.status(401).send({ type: error.type, message });
     } else {
       logger.error("AUTHORIZATION_UNKNOWN_ERROR", error);
       const err = new InvalidTokenError();
@@ -47,4 +62,44 @@ const auth = async (req, res, next) => {
     }
   }
 };
+
+// const auth = async (req, res, next) => {
+//   let token = req.header("Authorization");
+//   try {
+//     if (!token) {
+//       throw new MissingTokenError();
+//     }
+
+//     token = token.replace("Bearer ", "");
+//     const data = jwt.verify(token, JWT_PRIVATE_KEY, {
+//       algotithms: ["HS256"],
+//       issuer: [JWT_ISSUER],
+//     });
+//     const user = await User.findOne({ _id: data._id });
+//     if (!user) {
+//       throw new NotAuthorizedError();
+//     }
+//     req.user = user;
+//     req.token = token;
+//     logger.info("USER_AUTHORIZED", { email: user.email });
+//     next();
+//   } catch (error) {
+//     logger.info("AUTHORIZATION_FAILED", {
+//       errorName: error.name,
+//       errorMessage: error.message,
+//     });
+//     if (
+//       error instanceof NotAuthorizedError ||
+//       error instanceof MissingTokenError
+//     ) {
+//       res.status(401).send({ type: error.type, message: error.message });
+//     } else if (error instanceof APIError) {
+//       res.status(401).send({ type: error.type, message: error.message });
+//     } else {
+//       logger.error("AUTHORIZATION_UNKNOWN_ERROR", error);
+//       const err = new InvalidTokenError();
+//       res.status(401).send({ type: err.type, message: err.message });
+//     }
+//   }
+// };
 module.exports = auth;
