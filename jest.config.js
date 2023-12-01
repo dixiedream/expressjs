5bf1a918b5adc35fa8add1bca0d5bd35b51c47c6
Alessandro <dixiedream@hotmail.it>
2023-12-01 17:15:17 +0100

    jest and tsc init

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+/** @type {import('jest').Config} */
+const config = {
+  bail: 1,
+  clearMocks: true,
+  collectCoverageFrom: [
+    "!**/node_modules/**"
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.d.ts',
+    '.js'
+  ],
+  coverageReporters: [
+    'html',
+    'text'
+  ],
+  errorOnDeprecated: true,
+  setupFilesAfterEnv: [
+    "./tests/jest.setup.js"
+  ],
+  roots: [
+    "./src/",
+    "./tests/"
+  ],
+  testEnvironment: "node",
+  preset: 'ts-jest'
+}
+
+module.exports = config
