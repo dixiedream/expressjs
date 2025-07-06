6d9dc80b4932dea8da52dd9503ac8142457f7a87
Alessandro <dixiedream@hotmail.it>
2025-07-06 17:15:59 +0200

    fixed translations

--- src/config/localization.ts
+++ src/config/localization.ts
@@ -1,9 +1,6 @@
 import i18next from 'i18next'
 import Backend from 'i18next-fs-backend'
 import * as middleware from 'i18next-http-middleware'
-import path from 'node:path'
-
-const __dirname = import.meta.dirname
 
 i18next
   .use(Backend)
@@ -11,7 +8,7 @@ i18next
   .init({
     fallbackLng: 'en',
     backend: {
-      loadPath: path.join(__dirname, '/../locales/{{lng}}/translation.json')
+      loadPath: 'src/locales/{{lng}}/translation.json'
     }
   })
 
